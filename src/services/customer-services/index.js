9318a16fc6d512340ae5ae425e972a6fe255267d
Ashish Kachhadiya <ashishkachhadiya1493@gmail.com>
2024-11-20 22:31:26 +0530

    add index file import

--- src/services/customer-services/index.js
+++ src/services/customer-services/index.js
@@ -3,3 +3,4 @@ module.exports.orderService = require('./order.service');
 module.exports.paymentService = require('./payment.service');
 module.exports.reviewService = require('./review.service');
 module.exports.getArtistsService = require('./get-artists.service');
+module.exports.favoriteService = require('./favorite.service');
